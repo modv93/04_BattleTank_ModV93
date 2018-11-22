d3b35beadeacb5b72c8fc040c8492bd41bb42c9b
DESKTOP-4EO6GQD\shubh <Shubham.tapmod@gmail.com>
2018-11-22 05:52:01 +0530

    30 Fire, Projectile and Projectile BP setup

---
+++ BattleTanks/Source/BattleTanks/Public/Projectile.h
@@ -0,0 +1,28 @@
+// #Battle Tanks is an open world TPS developed and modified by Mod_V93
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Projectile.generated.h"
+
+UCLASS()
+class BATTLETANKS_API AProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AProjectile();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+	
+};
