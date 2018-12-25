da718d181a71f4aee044e2f1b8d2f0cbad45cc9f
Shubham <shubham.tapmod@gmail.com>
2018-12-25 17:03:34 +0530

    55 OnHit event added in CPP

--- BattleTanks/Source/BattleTanks/Private/TankTrack.cpp
+++ BattleTanks/Source/BattleTanks/Private/TankTrack.cpp
@@ -7,6 +7,18 @@ UTankTrack::UTankTrack()
 {
 	PrimaryComponentTick.bCanEverTick = true;
 }
+
+void UTankTrack::BeginPlay()
+{
+	Super::BeginPlay();
+	OnComponentHit.AddDynamic(this, &UTankTrack::OnHit);
+}
+
+void UTankTrack::OnHit(UPrimitiveComponent * HitComponent, AActor * OtherActor, UPrimitiveComponent * OtherComponent, FVector NormalImpulse, const FHitResult & Hit)
+{
+	UE_LOG(LogTemp, Warning, TEXT("On hit called "));
+}
+
 void UTankTrack::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction * ThisTickFunction)
 {
 	//Good practice to use this, however doesn't make any dfference here.
--- BattleTanks/Source/BattleTanks/Public/TankTrack.h
+++ BattleTanks/Source/BattleTanks/Public/TankTrack.h
@@ -26,5 +26,12 @@ public:
 
 private:	
 	UTankTrack();
+
+	virtual void BeginPlay() override;
+
 	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent * HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent,
+				FVector NormalImpulse, const FHitResult& Hit);
 };
